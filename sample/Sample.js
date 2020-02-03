f89d65b8db5bfbf2f3ff19e59b71b9beada8d46f
Wandalen <wandalen.me@gmail.com>
2020-02-03 21:38:15 +0200

    version 0.3.7

--- sample/Sample.js
+++ sample/Sample.js
@@ -1,7 +1,27 @@
 
-let _ = require( 'wTools' );
+let _ = require( 'wblueprint' );
 
 /**/
 
-...
+var Blueprint1 = _.blueprint
+({
+  field1 : 1,
+  field2 : 2,
+});
 
+var Blueprint2 = _.blueprint
+({
+  field2 : 22,
+  field3 : 33,
+  extend : _.define.extension( Blueprint1 ),
+});
+
+var instance = Blueprint2.construct();
+console.log( instance );
+/* log :
+{
+  field2 : 2,
+  field3 : 33,
+  field1 : 1,
+}
+*/
