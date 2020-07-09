d4863e70b8dcac838cf355fbde021ab1d33cd493
wandalen <wandalen.me@gmail.com>
2020-07-09 08:19:30 +0300

    .

--- proto/wtools/abase/l2_blueprint/Include.s
+++ proto/wtools/abase/l2_blueprint/Include.s
@@ -10,7 +10,7 @@
 if( typeof module !== 'undefined' )
 {
 
-  let _ = require( '../../../dwtools/Tools.s' );
+  let _ = require( '../../../wtools/Tools.s' );
 
   require( './l1/Definition.s' );
   require( './l1/Proto.s' );
