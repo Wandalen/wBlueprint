6fbd2f29627ef66c2fc575fe6144647c2220955d
wandalen <wandalen.me@gmail.com>
2020-12-10 10:36:55 +0200

    .

---
+++ proto/wtools/abase/l2_blueprint/l1/PropertyTransformers.s
@@ -0,0 +1,126 @@
+( function _PropertyTransformer_s_()
+{
+
+'use strict';
+
+let Self = _global_.wTools.property = _global_.wTools.property || Object.create( null );
+let _global = _global_;
+let _ = _global_.wTools;
+
+// --
+//
+// --
+
+function dstNotOwnFromDefinition()
+{
+  let routine = dstNotOwnFromDefinition;
+  routine.identity = { propertyMapper : true, propertyTransformer : true };
+  return routine;
+
+  function dstNotOwnFromDefinition( dstContainer, srcContainer, key )
+  {
+
+    if( Object.hasOwnProperty.call( dstContainer, key ) )
+    return;
+
+    if( Object.hasOwnProperty.call( dstContainer, Symbol.for( key ) ) )
+    return;
+
+    let srcElement = srcContainer[ key ];
+    if( _.definitionIs( srcElement ) )
+    dstContainer[ key ] = srcElement.valueGenerate( srcElement.val );
+    else
+    dstContainer[ key ] = srcElement;
+
+  }
+
+}
+
+dstNotOwnFromDefinition.identity = { propertyMapper : true, propertyTransformer : true, functor : true };
+
+//
+
+function dstNotOwnFromDefinitionStrictlyPrimitive()
+{
+  let routine = dstNotOwnFromDefinitionStrictlyPrimitive;
+  routine.identity = { propertyMapper : true, propertyTransformer : true };
+  return routine;
+
+  function dstNotOwnFromDefinitionStrictlyPrimitive( dstContainer, srcContainer, key )
+  {
+
+    if( Object.hasOwnProperty.call( dstContainer, key ) )
+    return;
+
+    if( Object.hasOwnProperty.call( dstContainer, Symbol.for( key ) ) )
+    return;
+
+    let srcElement = srcContainer[ key ];
+    if( _.definitionIs( srcElement ) )
+    {
+      dstContainer[ key ] = srcElement.valueGenerate( srcElement.val );
+    }
+    else
+    {
+      _.assert
+      (
+        !_.consequenceIs( srcElement ) && ( _.primitiveIs( srcElement ) || _.routineIs( srcElement ) ),
+        () => `${ _.strEntityShort( dstContainer ) } has non-primitive element "${ key }" use _.define.own instead`
+      );
+      dstContainer[ key ] = srcElement;
+    }
+
+  }
+
+}
+
+dstNotOwnFromDefinitionStrictlyPrimitive.identity = { propertyMapper : true, propertyTransformer : true, functor : true };
+
+// --
+// tools
+// --
+
+function mapSupplementOwnFromDefinition( dstMap, srcMap )
+{
+  return _.mapExtendConditional( _.property.mapper.dstNotOwnFromDefinition(), ... arguments );
+}
+
+//
+
+function mapSupplementOwnFromDefinitionStrictlyPrimitives( dstMap, srcMap )
+{
+  return _.mapExtendConditional( _.property.mapper.dstNotOwnFromDefinitionStrictlyPrimitive(), ... arguments );
+}
+
+// --
+// extension
+// --
+
+let Transformers =
+{
+
+  dstNotOwnFromDefinition,
+  dstNotOwnFromDefinitionStrictlyPrimitive,
+
+}
+
+_.property.transformersRegister( Transformers );
+
+let Extension =
+{
+
+  mapSupplementOwnFromDefinition,
+  mapSupplementOwnFromDefinitionStrictlyPrimitives,
+
+}
+
+_.mapExtend( _, Extension );
+
+// --
+// export
+// --
+
+if( typeof module !== 'undefined' )
+module[ 'exports' ] = _;
+
+})();
