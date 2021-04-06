a82fc2b2b68331a06ac488b44bde297165d3c169
wandalen <wandalen.me@gmail.com>
2021-04-06 16:04:24 +0300

    .

--- proto/wtools/abase/l2_blueprint.test/Blueprint.test.s
+++ proto/wtools/abase/l2_blueprint.test/Blueprint.test.s
@@ -5,7 +5,7 @@
 
 if( typeof module !== 'undefined' )
 {
-  let _ = require( '../../../wtools/Tools.s' );
+  let _ = require( '../../../node_modules/Tools' );
   require( '../../abase/l2_blueprint/Include.s' );
   // _.include( 'wReplicator' );
   _.include( 'wTesting' );
